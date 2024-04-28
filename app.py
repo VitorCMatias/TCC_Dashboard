3b2983d5b01cee1984dae24f5efe07e247799b2d
Vitor <vitorcosta.matias@usp.br>
2024-04-28 19:14:32 -0300

    app.py refactored

--- app.py
+++ app.py
@@ -9,25 +9,27 @@ from Map import GPS
 TODO
     - Aumentar a modularização do Map.py deixar o updade recebendo as coeedenadas x e y ou uma tupla
     - Deixar o mapa de calor recebendo uma lista de tuplas ou das coordenadas e das velocidades
-    - Tentar usar filas ou pilha ao invés de lista, colocar os dados que chegam em uma fila o primeiro item é a localização atual e os demais vão para o heatmap.
+    - Tentar usar filas ou pilha ao invés de lista, colocar os dados que chegam em uma fila o primeiro item é a
+     localização atual e os demais vão para o heatmap.
 '''
 
 df_front = get_coordinates(1)
 car_map = GPS()
 
-if 'dados' not in st.session_state:
-    st.session_state['dados'] = []
+if 'car_coordinates' not in st.session_state:
+    st.session_state['car_coordinates'] = []
 
-st.session_state['dados'].append(df_front.iloc[-1].tolist())
+st.session_state['car_coordinates'].append(df_front.iloc[-1].tolist())
 
-st_data = st_folium(car_map.get_map(), 
-    feature_group_to_add= [car_map.heat_map(st.session_state['dados']),car_map.update(df_front)],
-    height=400*1.2,
-    width=700*1.2,
+st_data = st_folium(
+    car_map.get_map(),
+    feature_group_to_add=[car_map.heat_map(st.session_state['car_coordinates']), car_map.update(df_front)],
+    height=400,
+    width=700,
 )
 
 auto_refresh = True
-number  = 2
+refresh_frequency = 2
 if auto_refresh:
-    time.sleep(number)
+    time.sleep(refresh_frequency)
     st.rerun()
