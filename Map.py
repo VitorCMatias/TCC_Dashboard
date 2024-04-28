b42a08cbac8ca892570d6441776ac18403baf330
Vitor <vitorcosta.matias@usp.br>
2024-04-28 19:17:06 -0300

    map.py refactored

--- Map.py
+++ Map.py
@@ -10,7 +10,7 @@ class GPS:
 
     def get_map(self):
         return self.map
-    
+
     def update(self, df_front):
         position = zip(df_front.latitude, df_front.longitude)
         for lat, lon in position:
@@ -20,7 +20,7 @@ class GPS:
                     location=(lat, lon),
                     radius=5,
                     color='#808080',
-                    fill = True,
+                    fill=True,
                     fill_opacity=1,
                     fill_color='#ffc72c'
                 )
@@ -28,15 +28,15 @@ class GPS:
             self.loc = (lat, lon)
 
         return self.figure
-    
+
     def get_locations(self):
         return self.loc
-    
-    def velocity_to_color(self, value, max_velocity=80):
+
+    def __velocity_to_color(self, value, max_velocity=80):
         normalized_value = value / max_velocity
         colormap = plt.get_cmap("coolwarm")
         rgb_color = colormap(normalized_value)
-        hex_color = '#%02x%02x%02x' % (int(rgb_color[0]*255), int(rgb_color[1]*255), int(rgb_color[2]*255))
+        hex_color = '#%02x%02x%02x' % (int(rgb_color[0] * 255), int(rgb_color[1] * 255), int(rgb_color[2] * 255))
 
         return hex_color
 
@@ -48,13 +48,12 @@ class GPS:
             fig.add_child(
                 folium.CircleMarker
                 (
-                    location=(data[i][0],data[i][1]),
+                    location=(data[i][0], data[i][1]),
                     radius=3,
                     opacity=0.7,
-                    color=self.velocity_to_color(data[i][2]),
-                    fill = True,
+                    color=self.__velocity_to_color(data[i][2]),
+                    fill=True,
                     fill_opacity=0.7,
                 )
             )
         return fig
-    
