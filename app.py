d49eef7b23e7ec8a22740651cbb7fbd021282ef1
Vitor <vitorcosta.matias@usp.br>
2024-04-29 17:32:58 -0300

    refactoring

--- app.py
+++ app.py
@@ -30,41 +30,30 @@ def get_date(df):
 df_front = mock_data.get()
 
 st.cache_data.clear()
-conn = st.connection('mock_data', type='sql',ttl=10)
+db_connection = st.connection('mock_data', type='sql',ttl=10)
 
 
-with conn.session as s:
+with db_connection.session as db_session:
     new_data = mock_table(timestamp=df_front.gps_datetime, latitude=df_front.latitude, longitude=df_front.longitude, speed=df_front.speed)
-    s.add(new_data)
-    s.commit()
+    db_session.add(new_data)
+    db_session.commit()
 
 car_map = GPS((-6.22444, 106.867111))
 
-# if 'car_data' not in st.session_state:
-#     st.session_state['car_data'] = []
+positions = db_connection.query('SELECT latitude, longitude, speed FROM mock_table')
+query_car_current_position = db_connection.query('SELECT latitude, longitude FROM mock_table WHERE id = (SELECT MAX(id) FROM mock_table)')
+car_current_position = get_position(query_car_current_position)
 
-# position = get_position(df_front)
-# speed = get_velocity(df_front)
-# timestamp = get_date(df_front)
-
-# st.session_state['car_data'].append([timestamp, position, speed])
-
-
-# car_data = st.session_state['car_data']
-
-query = conn.query('SELECT latitude, longitude, speed FROM mock_table')
-q2 = conn.query('SELECT latitude, longitude FROM mock_table WHERE id = (SELECT MAX(id) FROM mock_table)')
-position2 = get_position(q2)
-
-# st.write(position2)
 
 st_data = st_folium(
     car_map.get_map(),
-    feature_group_to_add=[car_map.heat_map(query), car_map.position_update(position2)],
+    feature_group_to_add=[car_map.heat_map(positions), car_map.position_update(car_current_position)],
     height=400,
     width=700,
 )
-q = conn.query('select timestamp, speed from mock_table')
+
+
+q = db_connection.query('select timestamp, speed from mock_table')
 fig = px.bar(q, x='timestamp', y='speed')
 
 st.plotly_chart(fig, use_container_width=True)
