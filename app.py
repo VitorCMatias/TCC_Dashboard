80d30587558cb7463ddfcc0e75b962d83efe36d7
Vitor <vitorcosta.matias@usp.br>
2024-05-01 19:55:15 -0300

    updates

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
-import numpy as np
 import streamlit as st
 from streamlit_folium import st_folium
 import time
 from Map import GPS
-import plotly.express as px
 import pandas as pd
 from Mock import Mock
+from Plot.Plot import bar_plot, line_plot
 
 
-def calculate_acceleration(df: pd.DataFrame) -> pd.Series:
+def calculate_acceleration(df: pd.DataFrame, time_colum: str = 'timestamp', speed_solum: str = 'speed') -> pd.DataFrame:
     """
-    Calculates acceleration from a DataFrame containing speed values.
+    Calcula a aceleração a partir de um DataFrame contendo valores de velocidade.
 
-    Args:
-        df (pd.DataFrame): A DataFrame with columns 'timestamp', 'speed' (in km/h).
-
-    Returns:
-        pd.Series: A Series containing acceleration values (in km/h^2).
+    @param df: Um DataFrame com as colunas 'timestamp', 'speed'.
+    @param time_colum:  Coluna do dataframe contendo os valores de tempo.
+    @param speed_solum: Coluna do dataframe contendo os valores de velocidade.
+    @return: O dataframe original contendo as colunas de aceleração adicionadas a ele.
     """
-    
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['time_interval_s'] = (df['timestamp'] - df['timestamp'].shift(1)).dt.total_seconds()
-    df['acceleration'] = (df['speed'] - df['speed'].shift(1)) / df['time_interval_s']
+
+    df[time_colum] = pd.to_datetime(df[time_colum])
+    df['time_interval_s'] = (df[time_colum] - df[time_colum].shift(1)).dt.total_seconds()
+    df['acceleration'] = (df[speed_solum] - df[speed_solum].shift(1)) / df['time_interval_s']
+
+    df.drop(columns=['time_interval_s'], inplace=True)
+    df.dropna(subset=['acceleration'], inplace=True)
 
     return df
-
 
 
 def get_position(df):
     position = df[['latitude', 'longitude']].apply(tuple, axis=1)
     return position.iloc[0]
-
 
 
 st.cache_data.clear()
@@ -40,32 +39,37 @@ mock.add()
 
 positions = mock.get_previous_positions()
 query_car_current_position = mock.get_current_position()
+speed_sample = mock.get_sample_speed()
 
-car_map = GPS((-6.22444, 106.867111))
 car_current_position = get_position(query_car_current_position)
 
+
+car_map = GPS((-6.22444, 106.867111),zoom=10)
 
 st_data = st_folium(
     car_map.get_map(),
     feature_group_to_add=[car_map.heat_map(positions), car_map.position_update(car_current_position)],
     height=400,
-    width=700,
+    width=700*2,
 )
 
-speed_sample = mock.get_sample_speed()
+df_acelletation = calculate_acceleration(speed_sample)
 
-mean_velocity = np.mean(speed_sample['speed'])
-fig = px.bar(speed_sample, x='timestamp', y='speed', title='Speed')
-fig.add_hline(y=mean_velocity, line_dash='dot', annotation_text=f'{mean_velocity:.2f}km/h', annotation_position='top right')
+st.plotly_chart(
+    bar_plot(df_acelletation, 'timestamp', 'speed', title='Velocidade em km/h', unit_of_measurement='km/h'),
+    use_container_width=True)
+
+st.plotly_chart(
+    bar_plot(df_acelletation, 'timestamp', 'acceleration', title='Aceleração em km/h²'),
+    use_container_width=True)
+
+st.plotly_chart(
+    line_plot(df_acelletation, 'timestamp', 'speed', y2='acceleration'),
+    use_container_width=True)
 
 
-st.plotly_chart(fig, use_container_width=True)
 
-fig_acel = px.bar(calculate_acceleration(speed_sample), x='timestamp', y='acceleration', title='acceleration')
-
-st.plotly_chart(fig_acel, use_container_width=True)
-
-st.dataframe( mock.get_all())
+st.dataframe(mock.get_all())
 
 auto_refresh = True
 refresh_frequency = 2
