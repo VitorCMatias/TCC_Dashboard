59a400564bca00e59c86faa09ab587ce74d39e4a
Vitor <vitorcosta.matias@usp.br>
2024-05-03 14:24:39 -0300

    sphinx setup

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CAN Monitor Dashboard'
+copyright = '2024, Vitor Matias'
+author = 'Vitor Matias'
+release = 'a1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+# sys.path.insert(0, os.path.abspath("../../"))
+
+
+extensions = ['myst_parser',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.coverage',
+              'sphinx.ext.napoleon',
+              'autoapi.extension']
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autoapi_dirs = ['../..']
+autodoc_typehints = 'both'
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. CAN Monitor Dashboard documentation master file, created by
+   sphinx-quickstart on Thu May  2 17:24:03 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CAN Monitor Dashboard's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
